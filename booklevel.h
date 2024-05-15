8ea9cb635e578716216cae2f9d00034246a2e5ff
SirVegetable <17wgs@queensu.ca>
2024-05-14 22:34:33 -0400

    the level info in the order book and the depth of the orderbook level

---
+++ booklevel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "utils.h"
+
+struct LevelDepth
+{
+    OrderPrice _price; 
+    OrderQuantity _quantity;    
+}; 
---
+++ orderbookleveldepth.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "booklevel.h"
+
+class BookDepth
+{
+    public:
+        using Book_Level_Depth = std::vector<LevelDepth>; 
+        BookDepth(); 
+        ~BookDepth();
+        const Book_Level_Depth& get_asks() const {return _asks;}; 
+        const Book_Level_Depth& get_buys() const {return _buys;}; 
+
+    private: 
+        Book_Level_Depth _asks;
+        Book_Level_Depth _buys; 
+
+}; 
