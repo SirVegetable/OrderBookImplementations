08ab089c2cf7252daaae0d24241d4b735d3b8315
SirVegetable <17wgs@queensu.ca>
2024-05-14 22:15:17 -0400

    utilities header for the orderbook

---
+++ utils.h
@@ -0,0 +1,15 @@
+#pragma once 
+
+#include <vector>
+
+enum class Side
+{
+    Buy, Sell
+}; 
+
+using OrderPrice = std::int32_t;
+using OrderQuantity = std::int32_t; 
+using OrderID = std::int64_t; 
+
+
+
