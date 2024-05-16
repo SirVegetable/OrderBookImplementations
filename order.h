5c064fa1529ab0d4f21cf9b2c879cb359b42d99a
SirVegetable <17wgs@queensu.ca>
2024-05-15 22:16:28 -0400

    finishing class

--- order.h
+++ order.h
@@ -2,27 +2,51 @@
 
 class Order{
     public:
-        Order() 
+        Order(Side side, OrderID id, OrderPrice price, OrderQuantity quantity, OrderQuantity remaining) : 
+            _side(side), order_id(id), order_quantity(quantity), remaining_order_quanity(remaining)
         {}
         ~Order()
         {}
+
+        OrderID get_order_id() const
+        {
+            return order_id; 
+        } 
+
+        Side get_side() const 
+        {
+            return _side; 
+        }
+
+        OrderPrice get_price() const
+        {
+            return order_price; 
+        }
+
+        OrderType get_order_type() const
+        {
+            return order_type; 
+        }
+
+        OrderQuantity get_initial_quantity() const
+        {
+            return order_quantity; 
+        }
+
+        OrderQuantity get_remaining_fill_quantity() const
+        {
+            return remaining_order_quanity; 
+        }
         
-        OrderID get_order_id(); 
+        OrderQuantity get_filled_quanity()
+        {
+            return order_quantity - remaining_order_quanity; 
+        }
 
-        Side get_side();
-
-        OrderPrice get_price(); 
-
-        OrderType get_order_type(); 
-
-        OrderQuantity get_initial_quantity(); 
-
-        OrderQuantity get_remaining_fill_quantity; 
-
-        void fill_order(); 
-
-
-
+        void fill_order()
+        {
+            
+        }
     
 
     private:
@@ -30,9 +54,7 @@ class Order{
         OrderPrice order_price; 
         OrderQuantity order_quantity;
         OrderID order_id;
-        OrderType order_type; 
-
-
-
+        OrderType order_type;
+        OrderQuantity remaining_order_quanity; 
 
 }; 
