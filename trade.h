1dd866eaeda28311fae32b874df0e37ffd060e58
SirVegetable <17wgs@queensu.ca>
2024-05-15 23:25:00 -0400

    finishing offtrade and adding the orderbook header file

--- trade.h
+++ trade.h
@@ -8,7 +8,14 @@ class Trade
         Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade): bid_trade(bidTrade), ask_trade(ask_trade)
         {}
 
-
+        const TradeInfo& get_bid_trade() const
+        {
+            return bid_trade; 
+        }
+        const TradeInfo& get_ask_trade() const
+        {
+            return ask_trade; 
+        }
     private:
         TradeInfo bid_trade; 
         TradeInfo ask_trade; 
