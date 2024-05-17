b993adae2911d4e0bed7fb7732171084b748ba03
SirVegetable <17wgs@queensu.ca>
2024-05-16 23:40:05 -0400

    filling out interface

--- orderbook.h
+++ orderbook.h
@@ -1,10 +1,23 @@
 #include <map> 
 #include "utils.h"
 #include "order.h"
+#include "trade.h"
+#include "modifyorder.h"
 
 class OrderBook
 {
-    public: 
+    public:
+        OrderBook(); 
+        ~OrderBook(); 
+        OrderBook(const OrderBook& )= delete;
+        void operator=(const OrderBook&) = delete; 
+        OrderBook(OrderBook&&) = delete; 
+        void operator=(OrderBook&&) = delete; 
+
+        Trades add_order(OrderPtr order);
+        Trades modify_order(OrderModify order);
+        void cancel_order(OrderID order_id);  
+
     private:
         struct OrderEntry
         {
@@ -14,7 +27,9 @@ class OrderBook
         std::map<OrderPrice,OrderPtrs, std::greater<OrderPrice>> bids;
         std::map<OrderPrice,OrderPtrs, std::less<OrderPrice>> asks;
         std::unordered_map<OrderID,OrderEntry> orders;
-        
-        
+        bool can_match(Side side, OrderPrice price); 
+        bool can_complete_fill(Side side, OrderPrice price, OrderQuantity quantity);
+        Trades match_orders(); 
+
 
 };
