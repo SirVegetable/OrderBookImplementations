1c04bfa3d9a63e8a704d6ddbdb755073f066982d
SirVegetable <17wgs@queensu.ca>
2024-05-16 23:18:41 -0400

    order entry object and bid ask trees

--- orderbook.h
+++ orderbook.h
@@ -1,7 +1,20 @@
-
+#include <map> 
+#include "utils.h"
+#include "order.h"
 
 class OrderBook
 {
     public: 
     private:
+        struct OrderEntry
+        {
+            OrderPtr order_object{nullptr}; 
+            OrderPtrs::iterator order_location; 
+        }; 
+        std::map<OrderPrice,OrderPtrs, std::greater<OrderPrice>> bids;
+        std::map<OrderPrice,OrderPtrs, std::less<OrderPrice>> asks;
+        std::unordered_map<OrderID,OrderEntry> orders;
+        
+        
+
 };
