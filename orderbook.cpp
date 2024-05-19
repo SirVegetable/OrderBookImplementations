afcdfaece565bc7ffbec9e08bfaa089ddb38c600
SirVegetable <17wgs@queensu.ca>
2024-05-19 17:19:05 -0400

    implemented the can fill function

--- orderbook.cpp
+++ orderbook.cpp
@@ -1,3 +1,4 @@
+
 #include "orderbook.h"
 
 OrderBook::OrderBook()
@@ -12,13 +13,58 @@ OrderBook::~OrderBook()
 
 bool OrderBook::can_match(Side side, OrderPrice price)
 {
+    if(side == Side::Sell)
+    {
+        if(asks.empty())
+        {
+            return false; 
+        }
+        const auto& [best_ask,_] = *bids.begin(); 
+        return price >= best_ask; 
+
+    }
+    if(side == Side::Buy)
+    {
+        if(asks.empty())
+        {
+            return false; 
+        }
+        const auto& [best_bid, _] = *asks.begin(); 
+        return best_bid;
+    }
 
 }
 
 bool OrderBook::can_complete_fill(Side side, OrderPrice price, OrderQuantity quantity)
 {
+    Trades trades; 
+    trades.reserve(orders.size()); 
+    
+    while(1)
+    {
+        if(bids.empty() || asks.empty())
+        {
+            break; 
+        }
+        auto& [bid_price,bids_] = *bids.begin(); 
+        auto& [ask_price, asks_] = *asks.begin();
+
+        if(bid_price < ask_price)
+        {
+            break; 
+        }
+        while(bids_.size(), asks_.size())
+        {
+            auto bid = bids_.front(); 
+            auto ask = asks_.front(); 
+
+        }
+
+    }
     
 }
+
+
 Trades OrderBook::match_orders()
 {
     
